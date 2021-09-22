89618a277e6865bd3f60f3efedb32271f9650e0e
Arnaldo Borda <aborda001@gmail.com>
2021-09-22 19:59:03 -0400

    Ejercicio6

---
+++ ProgramacionMauri/Tarea1/ejercicio6.c
@@ -0,0 +1,24 @@
+/*Que lea 10 números por teclado, los almacene en un array 
+*y muestre la suma y la multiplicación de todos.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
+
+int main()
+{
+	int array[MAX], sum = 0,i ;
+	long int  multiply = 1;
+	for (i = 0; i < MAX; ++i)
+	{
+		printf("Ingrese un numero:	\n");
+		scanf(" %d", array[i]);
+		sum += array[i];
+		multiply *= array[i];
+	}
+
+	printf("La suma de los numeros es: %d\n", sum );
+	printf("La multiplicacion de los numeros es: %d\n", multiply );
+	return 0;
+}
