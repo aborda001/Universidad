267adcb3e7bec6d98ae4baf61a990182a9712cf5
Arnaldo Borda <aborda001@gmail.com>
2021-10-03 19:04:56 -0300

    Ejercicio14

---
+++ ProgramacionMauri/Tarea1/ejercicio14.c
@@ -0,0 +1,45 @@
+/*Que rellene un array con 10 números aleatorios 
+*y luego busque un número concreto.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 5
+
+int main()
+{
+	/*Se utiliza el tiempo en segundos como semilla y 
+	 * de esa forma genera numeros aleatorios en cada ejecucion
+	 */
+	srand(time(NULL));
+	int array[MAX], searching;
+	int i;
+
+	printf("Numeros: ");
+	for (i = 0; i < MAX; ++i)
+	{
+		array[i] = rand() % 100;
+		printf("%d, ", array[i]);
+	}
+
+	printf("\nIngrese el numero a buscar: ");
+	scanf("%d", &searching);
+
+	for (i = 0; i < MAX; ++i)
+	{
+		if(array[i] == searching)
+		{
+			printf("El numero se encuentra en la posicion '%d'", i );
+			break;
+		}
+		else
+		{
+			if (i == MAX - 1)
+			{
+				printf("El numero no se encuentra en el array\n");
+			}
+		}
+	}
+	return 0;
+}
